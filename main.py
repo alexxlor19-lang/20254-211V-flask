43e1ea5fd4e9452be94e1caf68e08b96384bf786
alexxlor19-lang <alexxlor19@gmail.com>
2026-05-12 13:22:28 -0500

    template

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+base_de_datos =["jorge", "pepe"; "juanita"] #Modelos
+
+@app.route("/") #Controlador
 def hello():
-    return "Hello, World!"
+    return render_template(template_name_or_lista="index.html") #View
